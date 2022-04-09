67b92c541aa6530a4ec0b69df8a3d05b43ff4dca
Alexander DeRieux <alexander.derieux@gmail.com>
2022-04-09 19:14:40 -0400

    Add batch job example for Python

---
+++ example_run_python_job.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --job-name example_run_python_job
+#SBATCH --partition=p100_normal_q
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:2
+#SBATCH --ntasks 4
+#SBATCH --cpus-per-task 4
+#SBATCH --time=12:00:00
+#SBATCH --account="bigdata"
+#SBATCH --mail-user=$USER
+#SBATCH --mail-type=BEGIN,END,FAIL
+
+# Load modules.
+echo "Loading modules"
+module reset
+module load Anaconda3/2020.11
+
+# Run python command using Anaconda enviroment.
+CONDA_ENV_NAME="tf-p100"
+conda run -n ${CONDA_ENV_NAME} python -c "import tensorflow as tf; print(tf.config.list_physical_devices('GPU'))"
